006891debd9becebcf2d77e10baacb54823778c1
mizunofukusayou <145898250+mizunofukusayou@users.noreply.github.com>
2026-05-29 04:11:54 +0900

    init nix-darwin

--- flake.nix
+++ flake.nix
@@ -7,10 +7,20 @@
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { nixpkgs, home-manager, ... }:
+    {
+      nixpkgs,
+      home-manager,
+      nix-darwin,
+      ...
+    }:
     let
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -21,6 +31,10 @@
       homeConfigurations."mizunofukusayou" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [ ./home-manager/home.nix ];
+      };
+
+      darwinConfigurations."mizunofukusayounoMacBook-Air" = nix-darwin.lib.darwinSystem {
+        modules = [ ./nix-darwin/configuration.nix ];
       };
     };
 }
