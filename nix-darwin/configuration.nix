f109a612a9760a6318955883939ccec20d453fd4
mizunofukusayou <145898250+mizunofukusayou@users.noreply.github.com>
2026-05-29 23:01:39 +0900

    nix-darwin: sudoをタッチIDで

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -1,8 +1,9 @@
-{
-  ...
-}:
+{ ... }:
 {
   nixpkgs.hostPlatform = "aarch64-darwin";
   system.stateVersion = 6;
   nix.enable = false;
+
+  # タッチIDでsudoを許可する
+  security.pam.services.sudo_local.touchIdAuth = true;
 }
