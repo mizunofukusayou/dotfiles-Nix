4355d6f5c54c17e12da7f6e68a69246ee1bc8f28
mizunofukusayou <145898250+mizunofukusayou@users.noreply.github.com>
2026-07-21 01:53:59 +0900

    nix-darwin: add: nixの設定

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -2,7 +2,7 @@
 {
   nixpkgs.hostPlatform = "aarch64-darwin";
   system.stateVersion = 6;
-  nix.enable = false;
+  nix.enable = true;
 
   imports = [
     ./modules/env.nix
@@ -15,5 +15,17 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  nix.settings.auto-optimise-store = true; # ハードリンクでストアを最適化する
+  nix.settings = {
+    auto-optimise-store = true; # ハードリンクでストアを最適化する
+
+    experimental-features = "fetch-tree nix-command flakes"; # フレークと新しいNixコマンドを有効化する
+
+    bash-prompt-prefix = "(nix:$name)\\040"; # `nix-shell` や `nix develop` に入った際、プロンプトに環境名を表示
+
+    nix-path = [ "nixpkgs=flake:nixpkgs" ]; # 従来の `<nixpkgs>` の参照先を、モダンな Flakes の登録先に統一
+
+    sandbox-fallback = false; # サンドボックスが利用できない場合はビルドを失敗させ、環境の汚染を防ぐ
+
+    sandbox = true; # サンドボックスを有効化することで、ビルド環境の汚染を防ぐ
+  };
 }
