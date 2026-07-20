a60179cdc74ee664a9e56baf11a5c01b14fec983
mizunofukusayou <145898250+mizunofukusayou@users.noreply.github.com>
2026-07-20 12:22:03 +0900

    git: add: 設定の追加

--- home-manager/git/git.nix
+++ home-manager/git/git.nix
@@ -2,13 +2,25 @@
 {
   programs.git = {
     enable = true;
+
     settings = {
       user = {
         name = "mizunofukusayou";
         email = "145898250+mizunofukusayou@users.noreply.github.com";
       };
-      init.defaultBranch = "main";
+
+      init.defaultBranch = "main"; # デフォルトのブランチ名を main に設定
+      pull.rebase = true; # git pull 時に自動で rebase を行う
+
+      merge.conflictStyle = "zdiff3"; # コンフリクト時に「変更前（Base）」のコードも表示して比較しやすくする
+      rerere.enabled = true; # 一度解決したコンフリクトの手順を記憶し、同じ衝突を自動で解決する
+
+      rebase.autoStash = true; # 未コミットの変更があっても、自動で stash して rebase を実行する
+
+      log.date = "iso"; # git log の日時フォーマットを直感的な ISO 形式（YYYY-MM-DD）にする
+      push.autoSetupRemote = true; # 新しいブランチの初回 push 時に --set-upstream を自動で行う
     };
+
     ignores = [
       ".DS_Store"
       "*.swp"
