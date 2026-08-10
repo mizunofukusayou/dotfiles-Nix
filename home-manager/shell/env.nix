739e2b73d55fce5e9c0404b577fb7b795972b098
mizunofukusayou <145898250+mizunofukusayou@users.noreply.github.com>
2026-08-11 01:36:50 +0900

    shell: style

--- home-manager/shell/env.nix
+++ home-manager/shell/env.nix
@@ -1,22 +1,24 @@
 # 環境変数、パスの追加、エイリアスなど、シェルの環境をカスタマイズする
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 {
   xdg.enable = true;
 
-  home.language = {
-    base = "en_US.UTF-8";
-    ctype = "ja_JP.UTF-8";
-  };
+  home = {
+    language = {
+      base = "en_US.UTF-8";
+      ctype = "ja_JP.UTF-8";
+    };
 
-  home.sessionVariables = {
-    EDITOR = "nvim";
-    JAVA_HOME = "${pkgs.temurin-bin.home}";
-    MATLAB_JAVA = "$JAVA_HOME";
-  };
+    sessionVariables = {
+      EDITOR = "nvim";
+      JAVA_HOME = "${pkgs.temurin-bin.home}";
+      MATLAB_JAVA = "$JAVA_HOME";
+    };
 
-  home.shellAliases = {
-    dr = "defaults read";
-    yy = "pbcopy";
-    diff = "code -d";
+    shellAliases = {
+      dr = "defaults read";
+      yy = "pbcopy";
+      diff = "code -d";
+    };
   };
 }
