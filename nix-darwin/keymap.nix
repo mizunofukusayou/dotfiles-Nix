a8cfac1e8cc4995a8d396b314ca90f53939b6ae9
mizunofukusayou <145898250+mizunofukusayou@users.noreply.github.com>
2026-08-13 15:23:50 +0900

    keymap: デバイスの環境依存をなくした

--- nix-darwin/keymap.nix
+++ nix-darwin/keymap.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 let
   hexToInt = s: pkgs.lib.trivial.fromHexString s;
 
@@ -37,23 +37,9 @@ let
     (mkKeyMapping kana rightArrow)
   ];
 
-  # MacBook 本体の内蔵キーボードを特定するための条件。
-  # Apple Silicon 機では内蔵キーボードの VendorID/ProductID が
-  # どちらも 0x0 になり、他のデバイスとも重複してしまうため使えない。
-  # 代わりに、`hidutil list` で確認できる
-  # "Apple Internal Keyboard / Trackpad" の LocationID を使う。
-  #
-  # 注意: `hidutil list` の出力ヘッダーは "UsagePage"/"Usage" と表示されるが、
-  # --matching (--filter) で実際に使えるキー名は
-  # "PrimaryUsagePage" / "PrimaryUsage" (man hidutil 参照)。
-  # UsagePage 1 / Usage 6 = Generic Desktop / Keyboard のエンドポイント。
-  #
-  # 事前に以下を実行して、自分のマシンの LocationID に書き換えること:
-  #   hidutil list --matching '{"PrimaryUsagePage":1,"PrimaryUsage":6}'
-  internalKeyboardLocationID = "0xE6"; # ← 要確認・要書き換え
-
+  # Product 名で内蔵キーボードを指定。
   matching = builtins.toJSON {
-    LocationID = hexToInt internalKeyboardLocationID;
+    Product = "Apple Internal Keyboard / Trackpad";
     PrimaryUsagePage = 1;
     PrimaryUsage = 6;
   };
@@ -61,10 +47,6 @@ let
   mapping = builtins.toJSON { UserKeyMapping = userKeyMapping; };
 in
 {
-  # 標準の system.keyboard.enableKeyMapping / userKeyMapping は、
-  # 内部で `hidutil property --set ...` を --matching なしで実行するため、
-  # 接続されている全キーボード(外付けも含む)に適用されてしまう。
-  # そのため使用せず、--matching 付きの activation script を自前で定義する。
   system.keyboard.enableKeyMapping = false;
 
   system.activationScripts.postActivation.text = ''
